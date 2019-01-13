e7ad9415de70be7f6ce12c3e8333b191ce54b58e
Juju Liao <liaojulian@gmail.com>
2019-01-12 18:30:06 -0800

    Initialize Files
    Project Files

---
+++ src/main/java/io/github/mrbeezwax/itemplates/ItemPlates.java
@@ -0,0 +1,17 @@
+package io.github.mrbeezwax.itemplates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemPlates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
